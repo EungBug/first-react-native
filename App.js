0799c5670de2fdf2ed75d777f933bb8e619bbdf0
EungBug <eungb14@gmail.com>
2024-09-16 16:41:58 +0900

    날씨 api 연동

--- App.js
+++ App.js
@@ -1,19 +1,50 @@
+import * as Location from 'expo-location';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native';
-const { width: SCREEN_WIDTH } = Dimensions.get("window"); // Screen size
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window'); // Screen size
 
 export default function App() {
+  const [city, setCity] = useState('Loading...');
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+
+  const askPermission = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&APPID=${process.env.API_KEY}`
+    );
+    const json = await res.json();
+    console.log(json);
+  };
+
+  useEffect(() => {
+    askPermission();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
         contentContainerStyle={styles.weather}
         horizontal
         pagingEnabled
         showsHorizontalScrollIndicator={false}
-      > 
+      >
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}>Sunny</Text>
@@ -42,33 +73,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#3268a8"
+    backgroundColor: '#3268a8',
   },
   city: {
     flex: 1.2,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   cityName: {
     fontSize: 48,
-    fontWeight: "500",
-    color: "#fff"
+    fontWeight: '500',
+    color: '#fff',
   },
-  weather: {
-  },
+  weather: {},
   day: {
     width: SCREEN_WIDTH,
-    alignItems: "center",
+    alignItems: 'center',
   },
   temp: {
     marginTop: 30,
     fontSize: 178,
-    fontWeight: "600",
-    color: "#fff",
+    fontWeight: '600',
+    color: '#fff',
   },
   description: {
     marginTop: -30,
     fontSize: 60,
-    color: "#fff",
-  }
+    color: '#fff',
+  },
 });
